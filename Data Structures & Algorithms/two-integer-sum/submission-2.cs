a9feee288c613797f84d7f29b9104d4ad3236943
Leandro Costa <36852893+franciscostein@users.noreply.github.com>
2026-06-16 19:59:40 -0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        int[] result = new int[2] { 0, 0 };
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (i != j && nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
